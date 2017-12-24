e6a199b4f34eb05f5c20d5883ed57ae9cc12196e
吴智彬 <519002008@qq.com>
2017-12-24 15:30:46 +0800

    显示前台首页

--- project/Application/Home/Controller/IndexController.class.php
+++ project/Application/Home/Controller/IndexController.class.php
@@ -5,11 +5,30 @@ class IndexController extends CommonController
 {
     public function index()
     {
-    	// show($_SESSION['homeuser']);
-    	// // 读取用户余额
-    	// // $money = (int) M('users')->where('id='.$_SESSION['homeuser']['id'])->find()['balance'];
-    	// //分配数据        
-     //    // $this->assign('money',$money/100);
-    	$this->display();
+    	$userInfo = $_SESSION['homeuser'];
+
+    	if($userInfo){
+	        // 获取用户当前设备
+	        $did = $userInfo['did'];
+
+	        if(empty($did)){
+	        	// 请先绑定设备
+	        	$this->success('请先绑定设备...', 'Home/Devices/manage');
+	        }else{
+	        	// 查询用户设备设备码
+	        	$deviceInfo = M('Devices')
+	        	->where('pub_devices.id='.$did)
+	        	->join('pub_devices_statu ON pub_devices_statu.DeviceID=pub_devices.device_code')
+	        	->field('RawTDS,PureTDS,ReFlow,SumFlow')
+	        	->find();
+
+            	// 分配数据到模板
+            	$this->assign('deviceInfo',$deviceInfo);
+
+            	// 显示模板
+	        	$this->display();	
+	        }
+    	}
+
 	}
 }
