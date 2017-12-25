550517251601ca827444356fdd903e6af1f88317
吴智彬 <519002008@qq.com>
2017-12-25 10:11:43 +0800

    新增字段 Users表 安装地址

--- doc/架构设计/开发文档/微信关注开发文档/pub_users.sql
+++ doc/架构设计/开发文档/微信关注开发文档/pub_users.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50714
-Source Host           : localhost:3306
+Source Server         : wuzhibin.cn
+Source Server Version : 50173
+Source Host           : sdm359768304.my3w.com:3306
 Source Database       : sdm359768304_db
 
 Target Server Type    : MYSQL
-Target Server Version : 50714
+Target Server Version : 50173
 File Encoding         : 65001
 
-Date: 2017-12-12 16:25:23
+Date: 2017-12-25 10:10:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,6 +23,7 @@ CREATE TABLE `pub_users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
   `name` varchar(15) DEFAULT NULL COMMENT '用户名字',
   `phone` varchar(11) DEFAULT NULL COMMENT '手机号码',
+  `Installaddress` varchar(255) DEFAULT NULL COMMENT '安装地址',
   `user_status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '用户状态',
   `login_time` int(11) DEFAULT NULL COMMENT '最后登陆的时间',
   `login_ip` varchar(15) DEFAULT NULL COMMENT '最后登陆的IP地址',
@@ -30,10 +31,9 @@ CREATE TABLE `pub_users` (
   `open_id` varchar(50) NOT NULL COMMENT '关联微信信息表',
   PRIMARY KEY (`id`),
   KEY `name` (`name`,`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of pub_users
 -- ----------------------------
-INSERT INTO `pub_users` VALUES ('1', '', '', '1', '1', '1', '1', 'oXwY4t2gearWoyg8z19ygjD34vDk');
-INSERT INTO `pub_users` VALUES ('10', null, null, '1', '1512700661', '140.207.54.79', '1512700661', 'oXwY4t2gearWoyg8z19ygjD34vDk');
+INSERT INTO `pub_users` VALUES ('10', '测试', '13838381438', null, '1', '1513646091', '219.137.249.39', '1513646091', 'oXwY4t-9clttAFWXjCcNRJrvch3w');
