a0c14152decd87bde6719eaab064413875ec962b
ping <chenchangping@foxmail.com>
2017-12-29 11:55:19 +0800

    更新前台报修功能

--- project/Application/Home/Controller/MineController.class.php
+++ project/Application/Home/Controller/MineController.class.php
@@ -5,18 +5,18 @@ use Think\Controller;
 /**
  * 客户报修控制器
  * 
- * @author 潘宏钢 <619328391@qq.com>
+ * @author 陈昌平 <619328391@qq.com>
  */
 
-class RepairController extends CommonController 
+class MineController extends CommonController 
 {
 	/**
      * 报修页面
-     * @author 潘宏钢 <619328391@qq.com>
+     * @author 陈昌平 
      */
     public function index()
     {	
-        $id = array('id' => $_SESSION['homeuser']['did']);
+        $id = ['id' => $_SESSION['homeuser']['did']];
         $device_code = M('devices')->where($id)->find();
         if (IS_POST) {
             // 先处理图片
@@ -58,32 +58,5 @@ class RepairController extends CommonController
             $this->assign('code',$device_code);
             $this->display();
         }
-    }
-
-    // 图片上传
-    public function upload()
-    {
-        $upload = new \Think\Upload();// 实例化上传类
-        $upload->maxSize   =     3145728 ;// 设置附件上传大小
-        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $upload->rootPath  =     './Public/'; // 设置附件上传根目录
-        $upload->savePath  =     '/Pic/'; // 设置附件上传（子）目录
-        // 上传文件 
-        $info   =   $upload->upload();
-        if(!$info) {// 上传错误提示错误信息
-            return false;
-            // $this->error($upload->getError());
-        }else{
-            // 上传成功
-            foreach ($info as $file) {
-                // dump($info);die;
-                $pic[] = $file['savepath'].$file['savename'];
-            }
-            // $this->success('上传成功！');
-            return $pic;
-        }
-    }
-
-    
-    
+    }    
 }
