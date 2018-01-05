6accab5367ec4f13a8a588532a453729ce91cb46
吴智彬 <519002008@qq.com>
2018-01-05 09:40:53 +0800

    同步。。

--- project/Application/Home/Controller/WeixinEventController.class.php
+++ project/Application/Home/Controller/WeixinEventController.class.php
@@ -27,18 +27,19 @@ class WeixinEventController
 
 		if($xml){
             // 输出空字符串，回复微信服务器
-			echo '';
+			//echo '1';
 
 			// 转成php数组
 			$data = $this->toArray($xml);
 
             // 判断如果是关注事件
             if($data['Event'] == 'subscribe'){
-               
+            //echo 2;   
                 // 实例化微信信息类型
                 $Wechat = new WechatController;
                 // 调用填写微信信息的方法
                 $Wechat->add($data['FromUserName']);
+                
                 // file_put_contents('./add.txt', $xml);
                 exit;
             }
