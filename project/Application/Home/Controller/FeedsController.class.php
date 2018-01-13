d4ba847ba908885dea3c7f732f14f2eb2f76069d
吴智彬 <519002008@qq.com>
2018-01-13 16:55:04 +0800

    建议BUG修复

--- project/Application/Home/Controller/FeedsController.class.php
+++ project/Application/Home/Controller/FeedsController.class.php
@@ -22,7 +22,7 @@ class FeedsController extends CommonController
                 'content' => I('content'),
                 'uid' => $_SESSION['homeuser']['id'],
                 'addtime' => time(),
-                'did' => session('did'),
+                'did' => $_SESSION['homeuser']['did'],
             );
 
             // 实例化
