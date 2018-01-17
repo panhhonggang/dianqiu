42dd0c1b1bedec07fee141f3cffec57d0ab69eed
ping <chenchangping>
2018-01-17 11:25:24 +0800

    更新手机验证

--- project/Application/Home/Model/RepairModel.class.php
+++ project/Application/Home/Model/RepairModel.class.php
@@ -5,7 +5,7 @@ class RepairModel extends Model
 {
     protected $_validate = array(
         array('name','require','请留下您的真实姓名，便于联系反馈'), //默认情况下用正则进行验证 
-        array('phone','require','请留下您的联系电话，便于联系反馈'), //默认情况下用正则进行验证 
+        // array('phone','require','请留下您的联系电话，便于联系反馈'), //默认情况下用正则进行验证 
         array('phone','/^1[34578]\d{9}$/','电话号码格式不对',1,'regex'),
         array('address','require','请留下您的地址，以便处理问题'), //默认情况下用正则进行验证 
     );
