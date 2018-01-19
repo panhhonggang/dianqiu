67657aa6016ac52339961edf815353c5879a1be7
yi2018 <675218529@qq.com>
2018-01-19 15:09:53 +0800

    公版后台统计数据

--- project/Application/Admin/Controller/IndexController.class.php
+++ project/Application/Admin/Controller/IndexController.class.php
@@ -4,6 +4,28 @@ use Think\Controller;
 use Admin\Controller\CommonController;
 class IndexController extends CommonController {
     public function index(){
+    	if (IS_AJAX) {
+    		// 充值数额统计数量（本月列表显示）
+	    	$flows = D('Flow')->getCurrentMonth();
+
+	    	// 滤芯订单数量（以发货及未发货数量->以发货及未发货列表）
+	    	$order_filters = D('OrderFilter')->getAll();
+
+	    	// 保修数量统计->保修列表
+	    	$repairs = D('Repair')->getAll();
+
+	    	// 建议数量统计->建议列表
+	    	$feeds = D('Feeds')->getAll();
+
+	    	$data = [
+	    		'flows' => $flows,
+	    		'order_filters' => $order_filters,
+	    		'$repairs' => $repairs,
+	    		'feeds' => $feeds
+	    	];
+	    	$this->ajaxReturn($data);
+    	}
+ 
         $this->display('index');
     }
 
