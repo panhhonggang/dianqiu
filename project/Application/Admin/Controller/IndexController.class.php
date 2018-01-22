60a9578b9dcb1de96f789e788b1f50cf7adec125
yi2018 <675218529@qq.com>
2018-01-22 14:40:02 +0800

    更改统计

--- project/Application/Admin/Controller/IndexController.class.php
+++ project/Application/Admin/Controller/IndexController.class.php
@@ -8,12 +8,12 @@ class IndexController extends CommonController {
     		// 充值数额统计数量（本月列表显示）
 	    	$flows = D('Flow')->getTotalByEveryDay();
 
-	    	// 滤芯订单数量（以发货及未发货数量->以发货及未发货列表）
+	    	// 滤芯订单数量（已发货及未发货数量->以发货及未发货列表）
 	    	$order_filters = D('OrderFilter')
 	    									->field('distinct(order_id)')
 	    	                                ->select();
 	    	$order_filter['total'] = count($order_filters); 	
-	    	// 保修数量统计->保修列表
+	    	// 保修数量统计->保修列表  
 	    	$repairs['total'] = D('Repair')->count();
 
 	    	// 建议数量统计->建议列表
