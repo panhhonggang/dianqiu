fabe94803ae5a1063898bf9fcc9d805f1459ac50
yi2018 <675218529@qq.com>
2018-01-25 17:03:26 +0800

    添加设备列表设备已被绑定和未被绑定表格列

--- project/Application/Admin/Model/DevicesModel.class.php
+++ project/Application/Admin/Model/DevicesModel.class.php
@@ -43,7 +43,7 @@ class DevicesModel extends Model
             ->join("__BINDING__ bind ON d.id=bind.did", 'LEFT')
             ->join("__VENDORS__ vendors ON bind.vid=vendors.id", 'LEFT')
             ->join("__DEVICE_TYPE__ type ON d.type_id=type.id", 'LEFT')
-            ->field("statu.*,bind.*,d.id,d.device_code,type.*,vendors.*,d.name dname,d.phone,d.address")
+            ->field("statu.*,bind.*,d.id,d.device_code,type.*,vendors.*,d.name dname,d.phone,d.address,d.uid")
             ->order('d.id asc')
             ->limit($page->firstRow.','.$page->listRows)
             ->count();
@@ -60,7 +60,7 @@ class DevicesModel extends Model
             ->join("__BINDING__ bind ON d.id=bind.did", 'LEFT')
             ->join("__VENDORS__ vendors ON bind.vid=vendors.id", 'LEFT')
             ->join("__DEVICE_TYPE__ type ON d.type_id=type.id", 'LEFT')
-            ->field("statu.*,bind.*,d.device_code,type.*,vendors.*,d.name dname,d.phone,d.address,d.id")
+            ->field("statu.*,bind.*,d.device_code,type.*,vendors.*,d.name dname,d.phone,d.address,d.id,d.uid")
             ->order('d.id asc')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
