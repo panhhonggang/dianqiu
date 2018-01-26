6f76ea154b097c0f9225fb5be5b003b82a354594
陈志豪 <zero8297183@163.com>
2018-01-26 11:54:31 +0800

    微信分享调试

--- project/Public/Home/js/wx_share.js
+++ project/Public/Home/js/wx_share.js
@@ -8,7 +8,7 @@ var wxShare = function(appId,nonceStr,signature,timestamp){
 	// 	console.log(appId,nonceStr,signature,timestamp)
 	// }
 	wx.config({
-	    debug: false,
+	    debug: true,
 	    appId: appId,
 	    timestamp: timestamp,
 	    nonceStr: nonceStr,
@@ -22,13 +22,17 @@ var wxShare = function(appId,nonceStr,signature,timestamp){
 			'onMenuShareQZone'
 	    ]
 	});
+	var title = '净水物联网',
+		link = location.href,
+		desc = '欢迎使用点球净水物联网',
+		img = 'http://pub.dianqiukj.com/Public/Home/images/dianqiu.jpg';  //图标需要绝对路径
 
 	wx.ready(function(){
 		//分享到朋友圈
 		wx.onMenuShareTimeline({
-		    title: '欢迎使用点球净水物联网', // 分享标题
-		    link: 'pub.dianqiukj.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-		    imgUrl: '__PUBLIC__/Home/images/icon.jpg', // 分享图标
+		    title: title, // 分享标题
+		    link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		    imgUrl: img, // 分享图标
 		    success: function () {
 		    // 用户确认分享后执行的回调函数
 			}
@@ -36,10 +40,10 @@ var wxShare = function(appId,nonceStr,signature,timestamp){
 		
 		//分享给朋友
 		wx.onMenuShareAppMessage({
-			title: '欢迎使用点球净水物联网', // 分享标题
-			desc: '欢迎使用点球净水物联网', // 分享描述
-			link: 'pub.dianqiukj.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-			imgUrl: '__PUBLIC__/Home/images/icon.jpg', // 分享图标
+			title: title, // 分享标题
+			desc: desc, // 分享描述
+			link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			imgUrl: img, // 分享图标
 			type: '', // 分享类型,music、video或link，不填默认为link
 			dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
 			success: function () {
@@ -52,10 +56,10 @@ var wxShare = function(appId,nonceStr,signature,timestamp){
 		
 		// 获取“分享到QQ”按钮点击状态及自定义分享内容接口
 		wx.onMenuShareQQ({
-			title: '欢迎使用点球净水物联网', // 分享标题
-			desc: '欢迎使用点球净水物联网', // 分享描述
-			link: 'pub.dianqiukj.com/', // 分享链接
-			imgUrl: '__PUBLIC__/Home/images/icon.jpg', // 分享图标
+			title: title, // 分享标题
+			desc: desc, // 分享描述
+			link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			imgUrl: img, // 分享图标
 			success: function () {
 			// 用户确认分享后执行的回调函数
 			},
@@ -66,10 +70,10 @@ var wxShare = function(appId,nonceStr,signature,timestamp){
 		
 		// 获取“分享到腾讯微博”按钮点击状态及自定义分享内容接口
 		wx.onMenuShareWeibo({
-			title: '欢迎使用点球净水物联网', // 分享标题
-			desc: '欢迎使用点球净水物联网', // 分享描述
-			link: 'pub.dianqiukj.com/', // 分享链接
-			imgUrl: '__PUBLIC__/Home/images/icon.jpg', // 分享图标
+			title: title, // 分享标题
+			desc: desc, // 分享描述
+			link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			imgUrl: img, // 分享图标
 			success: function () {
 			// 用户确认分享后执行的回调函数
 			},
@@ -80,10 +84,10 @@ var wxShare = function(appId,nonceStr,signature,timestamp){
 		
 		// 获取“分享到QQ空间”按钮点击状态及自定义分享内容接口
 		wx.onMenuShareQZone({
-			title: '欢迎使用点球净水物联网', // 分享标题
-			desc: '欢迎使用点球净水物联网', // 分享描述
-			link: 'pub.dianqiukj.com/', // 分享链接
-			imgUrl: '__PUBLIC__/Home/images/icon.jpg', // 分享图标
+			title: title, // 分享标题
+			desc: desc, // 分享描述
+			link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			imgUrl: img, // 分享图标
 			success: function () {
 			// 用户确认分享后执行的回调函数
 			},
