212f5be23dda8bdb75f7328929a4374a24c8f6b6
washi <1986364669@qq.com>
2018-03-13 11:42:34 +0800

    公共导航条样式调整

--- project/Application/Home/Conf/config.php
+++ project/Application/Home/Conf/config.php
@@ -9,5 +9,9 @@ return array(
   	'APPSECRET' 	=> '1c39100b331713ae9e96a4e5eb470424',
   	'MCHID'     	=> '1480327842',
   	'KEY'       	=> 'CAA5EAE2CE5AC44A3F8930E6F127B423',
-  	'notify_url'	=> 'http://pub.dianqiukj.com/index.php/Home/PaymentSystem/notify.html'
+  	'notify_url'	=> 'http://pub.dianqiukj.com/index.php/Home/PaymentSystem/notify.html',
+
+  	// 开启layout模板布局
+  	'LAYOUT_ON'=>true,
+	'LAYOUT_NAME'=>'Layout/layout1',
 );
