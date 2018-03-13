25611247cc75a54a727ccd3ef1992b401d801381
xiaojuan <2536663785@qq.com>
2018-03-13 13:40:00 +0800

    接受数据

--- project/Public/Home/js/comment.js
+++ project/Public/Home/js/comment.js
@@ -1,15 +1,17 @@
-( function() {
+;( function() {
 	var doc = document.documentElement;
 	$("#contain").css({"height":doc.clientHeight, "width":doc.clientWidth});//获取窗口宽高
 	// 窗口改变#contain大小改变
 	window.onresize = function() {
+		window.location.reload();
 		$("#contain").css({"height":doc.clientHeight, "width":doc.clientWidth});
 	}
 	//layuiHint弹框提示封装
-	function layuiHint(text){
+	layuiHint = function(text){
 	        layui.use('layer', function(){
 	            var layer = layui.layer;
 	            layer.msg(text);
 	        });  
-	}
+	};
+	return layuiHint;
 }());
