792a168f70570bd88d158b9b5cea7e5ba02d80bd
meikoul <meikoul@163.com>
2018-03-13 09:19:24 +0800

    安装人员前台

--- project/Application/Home/Controller/PersonnelController.class.php
+++ project/Application/Home/Controller/PersonnelController.class.php
@@ -7,22 +7,51 @@ use \Org\Util\WeixinJssdk;
  */
 class PersonnelController extends Controller
 {
+
     /*
      * 安装人员登录页面
      */
-    public function  login() {
+    public function login()
+    {
         if (IS_POST) {
-            $where['phone'] = I('post.phone','','strip_tags');
-            $where['password'] = MD5(I('post.password','','strip_tags'));
+            $where['phone'] = I('post.phone', '', 'strip_tags');
+            $where['password'] = MD5(I('post.password', '', 'strip_tags'));
             $perSonnel = D('Personnel');
-            $get_info  = $perSonnel->getInfo($where);
+            $get_info = $perSonnel->getInfo($where);
             if ($get_info['code'] == 403) {
                 $this->error($get_info['message']);
             }
             if ($get_info['code'] == 200) {
+                session('pid', $get_info['data']['id']);
                 $this->success($get_info['message']);
             }
 
         }
+    }
+
+    /*
+     * 安装人员登录成功首页
+     */
+    public function index()
+    {
+        //个人资料
+        $info = M('personnel')->where('id', '=', session('pid'))->find();
+
+        $this->assign('info', $info);
+
+    }
+
+    /*
+     * 安装设备添加
+     */
+    public function per_detail()
+    {
+        $map['personnel_id'] = 23;
+        $map['id'] = 2;
+        //查询数据
+        $perSonnel_info = D('Personnel');
+        $info = $perSonnel_info->per_detail();
+
+
     }
 }
--- project/Application/Home/Model/PersonnelModel.class.php
+++ project/Application/Home/Model/PersonnelModel.class.php
@@ -7,14 +7,30 @@ class PersonnelModel extends Model
         array('phone','require','登录账号不能为空'), //默认情况下用正则进行验证
         array('password','require','密码不能为空'), //默认情况下用正则进行验证
     );
-    // 账号密码是否匹配
-    public function getInfo($map)
+    /*
+     * 安装人员账号密码验证
+     */
+    protected function getInfo($map)
     {
         $info = M('personnel')->where($map)->find();
         if (empty($info)) {
             return ['code'=>403,'message'=>'账号或者密码错误'];
         } else {
-                return ['code'=>200,'message'=>'登录成功'];
+            return ['code'=>200,'message'=>'登录成功','data'=>$info];
+        }
+    }
+
+    /*
+     * 安装设备详情
+     */
+    public function per_detail($map) {
+        $info = M('work')->field('dcode')->where($map)->find();
+
+        if ($info) {
+
+            return ['code'=>200,'data'=>$info];
+        } else {
+            return ['code'=>403,'data'=>'数据有误'];
         }
     }
 }
