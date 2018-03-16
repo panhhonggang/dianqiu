c64da199ed7de70410af4db8e411454e807dbb5b
washi <1986364669@qq.com>
2018-03-16 11:42:49 +0800

    服务人员登录页面样式更新

--- project/Public/Home/js/comment.js
+++ project/Public/Home/js/comment.js
@@ -1,10 +1,7 @@
 ;( function() {
 	var doc = document.documentElement;
 	$("#contain").css({"height":doc.clientHeight, "width":doc.clientWidth});//获取窗口宽高
-	// 窗口改变#contain大小改变
-	window.onresize = function() {
-		$("#contain").css({"height":doc.clientHeight, "width":doc.clientWidth});
-	}
+
 	//layuiHint弹框提示封装
 	layuiHint = function(text){
 	        layui.use('layer', function(){
