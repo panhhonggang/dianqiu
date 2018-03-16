e320e2f3e043bf7486380dfb24a2c9b77745e5ab
washi <1986364669@qq.com>
2018-03-16 11:32:00 +0800

    js更新

--- project/Public/Home/js/comment.js
+++ project/Public/Home/js/comment.js
@@ -3,7 +3,6 @@
 	$("#contain").css({"height":doc.clientHeight, "width":doc.clientWidth});//获取窗口宽高
 	// 窗口改变#contain大小改变
 	window.onresize = function() {
-		// window.location.reload();
 		$("#contain").css({"height":doc.clientHeight, "width":doc.clientWidth});
 	}
 	//layuiHint弹框提示封装
