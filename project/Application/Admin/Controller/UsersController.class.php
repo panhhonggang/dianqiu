7d325de4921230e219b19bf5b7b9a89c55b773e8
搞事Boy <lzdong@foxmail.com>
2018-03-20 16:17:15 +0800

    修复用户列表

--- project/Application/Admin/Controller/UsersController.class.php
+++ project/Application/Admin/Controller/UsersController.class.php
@@ -96,7 +96,7 @@ class UsersController extends CommonController
             ->join('__WECHAT__ w ON u.open_id=w.open_id', 'LEFT')
             ->join('__CURRENT_DEVICES__ cd ON u.id=cd.uid', 'LEFT')
             ->join('__DEVICES__ d ON cd.did=d.id', 'LEFT')
-            ->join('__BINDING__ bd ON d.id = bd.did ')
+            ->join('__BINDING__ bd ON d.id = bd.did ', 'LEFT')
             ->field('d.device_code,d.name,d.address,d.phone,w.*,u.*,cd.uid,cd.did,d.updatetime')
             ->count();
         $page  = new \Think\Page($total,10);
@@ -109,7 +109,7 @@ class UsersController extends CommonController
             ->join('__WECHAT__ w ON u.open_id=w.open_id', 'LEFT')
             ->join('__CURRENT_DEVICES__ cd ON u.id=cd.uid', 'LEFT')
             ->join('__DEVICES__ d ON cd.did=d.id', 'LEFT')
-            ->join('__BINDING__ bd ON d.id = bd.did ')
+            ->join('__BINDING__ bd ON d.id = bd.did ', 'LEFT')
             ->field('d.device_code,d.name,d.address,d.phone,w.*,u.*,cd.uid,cd.did,d.updatetime')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
