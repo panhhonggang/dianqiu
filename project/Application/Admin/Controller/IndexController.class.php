b05077ab5a09de909db303cee7818d781247b2e9
yi <675218529@qq.com>
2018-03-27 11:32:11 +0800

    后台bug修改

--- project/Application/Admin/Controller/IndexController.class.php
+++ project/Application/Admin/Controller/IndexController.class.php
@@ -8,6 +8,7 @@ class IndexController extends CommonController {
     		// 充值数额统计数量 （本月列表显示）
 	    	$flows = D('Flow')->getTotalByEveryDay();
 
+			$devices = D('Devices')->getTotalByEveryDay();
 	    	// 滤芯订单数量（已发货及未发货数量->以发货及未发货列表）
 	    	$order_filters = D('Orders')
 	    						->join('pub_express_information ON pub_orders.express_id = pub_express_information.id')
@@ -22,7 +23,8 @@ class IndexController extends CommonController {
 	    	$feeds['total'] = D('Feeds')->count();
 
 	    	$data = [
-	    		'flows' => $flows,
+				'flows' => $flows,
+				'devices'=> $devices,
 	    		'order_filters' => $order_filter,
 	    		'repairs' => $repairs,
 	    		'feeds' => $feeds
