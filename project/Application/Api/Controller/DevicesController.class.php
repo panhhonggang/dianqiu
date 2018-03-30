3e9dd53a45614d6c95c1ff30b3bc11389abf08e7
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-03-30 19:41:37 +0800

    梁康伦的修改—安卓设备录入产品接口修改

--- project/Application/Api/Controller/DevicesController.class.php
+++ project/Application/Api/Controller/DevicesController.class.php
@@ -25,24 +25,57 @@ class DevicesController extends Controller
             //接收安卓端数据
             //设备code码
             $data['device_code'] = $_POST['device_code'];
-            //经销商id
-            $data['uid'] = $_POST['uid'];
+
             //设备产品类型
-            $data['type_id'] = $_POST['type_id'];
+            $data['type_id'] = $_POST['type_id'] ? $_POST['type_id']:'';
 
-            //去除可能为空的值
-            $ndata = array_filter($data);
+            //经销商id
+            $vdata['vid'] = $_POST['uid'] ? $_POST['uid']:'';
 
+            $vdata['addtime'] = time();
+
+            
             //判断库里有没有这个设备编码
-            $boolean = $devices->where('device_code = '.$data['device_code'])->find();
+            $did = $devices->where('device_code = '.$data['device_code'])->field('id')->find()['id'];
 
-            if ($boolean) {
-                //数据进行修改
-                $bool = $devices->where('device_code = '.$data['device_code'])->save($ndata);
+
+            if ($did) {
+
+                //查找经销商是否存在
+                $vid = M('Binding')->where('did = '.$did)->field('vid')->find()['vid'];
+
+                if ($vid) {
+                    $bool = M('Binding')->where('did = '.$did)->save($vdata);
+                    $bool = $devices->where('device_code = '.$data['device_code'])->save($data);
+
+                } else {
+                    //数据进行修改
+                    $vdata['did'] = $did;
+                    $bool = M('Binding')->add($vdata);
+                    // echo M('Binding')->getLastSql();
+                    // dump($bool);die;
+                    $bool = $devices->where('device_code = '.$data['device_code'])->save($data);
+                }
+                
             } else {
+
+                // echo 111;die;
                 //数据写入
-                $bool = $devices->add($ndata);
+                $bool = $devices->add($data);
+                
+                $vdata['did'] = $devices->where('device_code = '.$data['device_code'])->field('id')->find()['id'];
+                
+                $bool = M('Binding')->add($vdata);
+                
             }
+
+            //返回个安卓的数据
+            $ndata['device_code'] = $data['device_code'];
+            $ndata['uid'] = $vdata['vid'];
+            $ndata['type_id'] = $data['type_id'];
+
+            //去除空值
+            array_filter($ndata);
 
             if($bool) {
                 echo json_encode($ndata);
