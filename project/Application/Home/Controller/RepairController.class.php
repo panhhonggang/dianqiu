6ef5e0c8717568bba8e9ba1cd5bea44cc8f60952
李振东 <lzdong@foxmail.com>
2018-05-05 14:52:44 +0800

    雷振东的修改—文件夹权限更新

--- project/Application/Home/Controller/RepairController.class.php
+++ project/Application/Home/Controller/RepairController.class.php
@@ -83,7 +83,7 @@ class RepairController extends CommonController
 
         $dir =rtrim(THINK_PATH,"ThinkPHP/").'/Public'.$path_info;
         if(!is_dir($dir)){
-            mkdir($dir,'0777',true);
+            mkdir($dir,0777,true);
         }
         $path_info = $path_info.'/'.$file;
 
